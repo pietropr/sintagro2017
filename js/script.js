b3fed7ab9b6d9d4668a6d6e4d1c87599f1cb515e
pietropr <pintonpietro@gmail.com>
2017-03-13 22:46:02 -0300

    Função do menu small na rolagem/ SCSS utilidades

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
+    function menu_size(){
+        if($(document).scrollTop()> 39) {
+            $(".site-header").addClass("small");
+        } else{
+            $(".site-header").removeClass("small");
+        }
+    }
 
+    $(window).scroll(function() {
+        menu_size();
+    })
 });
